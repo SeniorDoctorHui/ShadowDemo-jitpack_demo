b6e1919eb70f6f121a5167b298a327ee236abc90
LiZhuHui <nalanrumeng1113@163.com>
2019-05-06 16:12:11 +0800

    添加Path测试CustomView和Paint的PathDashPathEffect相关使用方法

---
+++ app/src/main/java/test/svn/huige/com/shadowdemo/CustomView.java
@@ -0,0 +1,70 @@
+package test.svn.huige.com.shadowdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * 所属系统: ShadowDemo-master-master
+ * 所属模块:  ${TODO}
+ * 功能描述:  ${TODO}
+ * 创建时间: 2019/5/6 15:59
+ * 维护人: 李主辉
+ * Copyright @ Jerrisoft 2019. All rights reserved.
+ * ┌─────────────────────────────────────────────────────┐
+ * │ 此技术信息为本公司机密信息，未经本公司书面同意禁止向第三方披露．│
+ * │ 版权所有：杰人软件(深圳)有限公司                          │
+ * └─────────────────────────────────────────────────────┘
+ */
+public class CustomView extends View {
+    private int mWidth;
+    private int mHeight;
+    private Paint mPaint;
+    public CustomView(Context context) {
+        this(context,null);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();             // 创建画笔
+        mPaint.setColor(Color.BLACK);           // 画笔颜色 - 黑色
+        mPaint.setStyle(Paint.Style.STROKE);    // 填充模式 - 描边
+        mPaint.setStrokeWidth(10);              // 边框宽度 - 10
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.translate(mWidth / 2, mHeight / 2);  // 移动坐标系到屏幕中心(宽高数据在onSizeChanged中获取)
+        //相对于上一个参考点移动
+        canvas.translate(100,100);
+
+        Path path = new Path();                     // 创建Path
+
+        path.lineTo(200, 200);                      // lineTo
+        path.lineTo(200,0);
+
+        canvas.drawPath(path, mPaint);
+    }
+}
--- app/src/main/java/test/svn/huige/com/shadowdemo/SetPolyToPoly.java
+++ app/src/main/java/test/svn/huige/com/shadowdemo/SetPolyToPoly.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathMeasure;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -68,16 +69,16 @@ public class SetPolyToPoly extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_MOVE:
                 float tempX = event.getX();
                 float tempY = event.getY();
 
                 // 根据触控位置改变dst
-                for (int i=0; i<testPoint * 2; i+=2 ) {
-                    if (Math.abs(tempX - dst[i]) <= triggerRadius && Math.abs(tempY - dst[i+1]) <= triggerRadius){
-                        dst[i]   = tempX-100;
-                        dst[i+1] = tempY-100;
+                for (int i = 0; i < testPoint * 2; i += 2) {
+                    if (Math.abs(tempX - dst[i]) <= triggerRadius && Math.abs(tempY - dst[i + 1]) <= triggerRadius) {
+                        dst[i] = tempX - 100;
+                        dst[i + 1] = tempY - 100;
                         break;  // 防止两个点的位置重合
                     }
                 }
@@ -90,7 +91,7 @@ public class SetPolyToPoly extends View {
         return true;
     }
 
-    public void resetPolyMatrix(int pointCount){
+    public void resetPolyMatrix(int pointCount) {
         mPolyMatrix.reset();
         // 核心要点  dst 目标数组 dst [x,y]，存储内容为一组点
         mPolyMatrix.setPolyToPoly(src, 0, dst, 0, pointCount);
@@ -98,7 +99,7 @@ public class SetPolyToPoly extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.translate(100,100);
+        canvas.translate(100, 100);
 
         // 绘制坐标系
 //        CanvasAidUtils.set2DAxisLength(300, 0, 1200, 0);
@@ -108,24 +109,93 @@ public class SetPolyToPoly extends View {
         canvas.drawBitmap(mBitmap, mPolyMatrix, null);
 
         float[] dst = new float[8];
-        mPolyMatrix.mapPoints(dst,src);
+        mPolyMatrix.mapPoints(dst, src);
 
         // 绘制触控点
-        for (int i=0; i<testPoint*2; i+=2 ) {
-            canvas.drawPoint(dst[i], dst[i+1],pointPaint);
+        for (int i = 0; i < testPoint * 2; i += 2) {
+            canvas.drawPoint(dst[i], dst[i + 1], pointPaint);
         }
-        Paint paint = new Paint();
-        paint.setStrokeWidth(20);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.BLACK);
-        Path path_dash = new Path();
-        path_dash.lineTo(0, 1020);
+//        Paint paint = new Paint();
+//        paint.setStrokeWidth(20);
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setColor(Color.BLACK);
+//        Path path_dash = new Path();
+//        path_dash.lineTo(0, 1020);
+//
+//        canvas.save();
+//        canvas.translate(880, 100);
+//        paint.setPathEffect(new DashPathEffect(new float[]{200, 200}, 0));
+//        canvas.drawPath(path_dash, paint);
+//        canvas.restore();
 
-        canvas.save();
-        canvas.translate(880, 100);
-        paint.setPathEffect(new DashPathEffect(new float[]{200, 200}, 0));
-        canvas.drawPath(path_dash, paint);
-        canvas.restore();
+        // 画笔初始设置
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+        RectF rectF = new RectF(0, 0, 50, 50);
+
+        // 方形
+        Path rectPath = new Path();
+        rectPath.addRect(rectF, Path.Direction.CW);
+
+        // 圆形 椭圆
+        Path ovalPath = new Path();
+        ovalPath.addOval(rectF, Path.Direction.CW);
+
+        // 子弹形状
+        Path bulletPath = new Path();
+        bulletPath.lineTo(rectF.centerX(), rectF.top);
+        bulletPath.addArc(rectF, -90, 180);
+        bulletPath.lineTo(rectF.left, rectF.bottom);
+        bulletPath.lineTo(rectF.left, rectF.top);
+
+        // 星星形状
+        PathMeasure pathMeasure = new PathMeasure(ovalPath, false);
+        float length = pathMeasure.getLength();
+        float split = length / 5;
+        float[] starPos = new float[10];
+        float[] pos = new float[2];
+        float[] tan = new float[2];
+        for (int i = 0; i < 5; i++) {
+            pathMeasure.getPosTan(split * i, pos, tan);
+            starPos[i * 2 + 0] = pos[0];
+            starPos[i * 2 + 1] = pos[1];
+        }
+        Path starPath = new Path();
+        starPath.moveTo(starPos[0], starPos[1]);
+        starPath.lineTo(starPos[4], starPos[5]);
+        starPath.lineTo(starPos[8], starPos[9]);
+        starPath.lineTo(starPos[2], starPos[3]);
+        starPath.lineTo(starPos[6], starPos[7]);
+        starPath.lineTo(starPos[0], starPos[1]);
+        Matrix matrix = new Matrix();
+        matrix.postRotate(-90, rectF.centerX(), rectF.centerY());
+        starPath.transform(matrix);
+
+
+        canvas.translate(0, 350);
+        // 绘制分割线 - 方形
+        canvas.translate(0, 100);
+        paint.setPathEffect(new PathDashPathEffect(rectPath, rectF.width() * 1.5f, 0, PathDashPathEffect.Style.TRANSLATE));
+        canvas.drawLine(0, 0, 1200, 0, paint);
+
+        // 绘制分割线 - 圆形
+        canvas.translate(0, 100);
+        paint.setPathEffect(new PathDashPathEffect(ovalPath, rectF.width() * 1.5f, 0, PathDashPathEffect.Style.TRANSLATE));
+        canvas.drawLine(0, 0, 1200, 0, paint);
+
+        // 绘制分割线 - 子弹型
+        canvas.translate(0, 100);
+        paint.setPathEffect(new PathDashPathEffect(bulletPath, rectF.width() * 1.5f, 0, PathDashPathEffect.Style.TRANSLATE));
+        canvas.drawLine(0, 0, 1200, 0, paint);
+
+        // 绘制分割线 - 星型
+        canvas.translate(0, 100);
+        paint.setPathEffect(new PathDashPathEffect(starPath, rectF.width() * 0.5f, 0, PathDashPathEffect.Style.ROTATE));
+        canvas.drawLine(0, 0, 120, 0, paint);
+
+
     }
 
     public void setTestPoint(int testPoint) {
